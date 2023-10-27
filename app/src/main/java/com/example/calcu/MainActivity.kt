3d725f09ab791e218d3ba2d0441b0a0d13234e31
WINDOW NOTIFICATION <abhitanshusrivastava10@gmail.com>
2023-10-27 12:44:05 +0530

    Calc

--- app/src/main/java/com/example/calcu/MainActivity.kt
+++ app/src/main/java/com/example/calcu/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.calcu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.calcu.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 
@@ -9,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -75,5 +79,6 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
     }
 }
